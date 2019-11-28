f3ddd876a77695b00e0386e5c232140bcb137f72
Roman Strukov <rm.strukov@ined.ru>
2019-11-28 11:23:09 +0300

    Class Test created

---
+++ spring-01-homework/src/main/java/ru/strukov/testing/domain/Test.java
@@ -0,0 +1,10 @@
+package ru.strukov.testing.domain;
+
+import java.util.Set;
+
+public class Test {
+    private Student student;
+    private Set<TestQuestion> questions;
+    private int questionsQuantity;
+    private int rightAnswers;
+}
