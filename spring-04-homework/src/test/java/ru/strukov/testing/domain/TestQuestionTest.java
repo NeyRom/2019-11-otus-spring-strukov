66ae3ac5c889479328028e8a50f0f22249d2dfb3
roman <neyrom@yandex.ru>
2020-04-14 18:06:17 +0300

    spring-04-homework merge

---
+++ spring-04-homework/src/test/java/ru/strukov/testing/domain/TestQuestionTest.java
@@ -0,0 +1,48 @@
+package ru.strukov.testing.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Created by Roman Strukov on 18.12.2019.
+ */
+@DisplayName("Проверка сервиса TestQuestion")
+class TestQuestionTest {
+    private TestQuestion testQuestion;
+
+    @BeforeEach
+    void setUp() {
+        testQuestion = new TestQuestion();
+        testQuestion.setQuestion("Год основания Москвы");
+        String[] answers = new String[] {"1147", "1941", "1812", "1649"};
+        testQuestion.setAnswers(new ArrayList<>(Arrays.asList(answers)));
+        testQuestion.setRightAnswer("1147");
+    }
+
+    @DisplayName("Корректно возвращает вопрос")
+    @Test
+    void shouldGetQuestion() {
+        assertEquals("Год основания Москвы", testQuestion.getQuestion());
+    }
+
+    @DisplayName("Корректно возвращает варианты ответов")
+    @Test
+    void shouldGetAnswers() {
+        List<String> answers = testQuestion.getAnswers();
+        assertThat(answers).isNotEmpty().hasSize(4).contains("1812");
+    }
+
+    @DisplayName("Корректно возвращает правильный ответ")
+    @Test
+    void shouldGetRightAnswer() {
+        assertEquals("1147", testQuestion.getRightAnswer());
+    }
+}
